28fadc13b43f60c09a1ab82b87feb527ba4c4a70
sonhm2329 <hoangminhson29102001@gmail.com>
2023-01-07 15:14:05 +0700

    demo sound

--- test_play_sound_v2.py
+++ test_play_sound_v2.py
@@ -1,6 +1,23 @@
 from playsound import playsound
+import time
+import threading
+import multiprocessing as mp
 
 
+alarm = False
+tempThread = None
+
+def sound():
+    print("Play")
+    global alarm
+    playsound("violence_sound_1.wav")
+    alarm = False
 
 for i in range(10):
-    playsound("violence_sound_1.wav")
+    time.sleep(0.18)
+    print(i)
+    if not alarm:
+        alarm = True
+        threading.Thread(target=sound).start()
+
+    
--- test_playing_sound.py
+++ test_playing_sound.py
@@ -3,24 +3,7 @@ import wave
 import time
 from pynput import keyboard
 
-paused = False    # global to track if the audio is paused
-def on_press(key):
-    global paused
-    print (key)
-    if key == keyboard.Key.space:
-        if stream.is_stopped():     # time to play audio
-            print ('play pressed')
-            stream.start_stream()
-            paused = False
-            return False
-        elif stream.is_active():   # time to pause audio
-            print ('pause pressed')
-            stream.stop_stream()
-            paused = True
-            return False
-    return False
 
-toggle_sound = 1
 # you audio here
 wf = wave.open('violence_sound_1.wav', 'rb')
 
@@ -33,35 +16,45 @@ def callback(in_data, frame_count, time_info, status):
     return (data, pyaudio.paContinue)
 
 # open stream using callback
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+
+
+for i in range(100):
+    
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                 channels=wf.getnchannels(),
                 rate=wf.getframerate(),
                 output=True,
                 stream_callback=callback)
-
+    stream.start_stream()
+    time.sleep(0.18)
+    stream.close()
+    
+    
 # start the stream
-stream.start_stream()
+# stream.start_stream()
 
-count = 0
-
-while stream.is_active() or paused==True:
-    count +=1
-    print(count)
-    time.sleep(1)
-    if(count % 5) == 0:
-        stream.stop_stream()
-        stream.close()
-        wf.close()
-        if toggle_sound == 1:
-            toggle_sound = 2
-        else:
-            toggle_sound = 1
-        wf = wave.open(f"violence_sound_{toggle_sound}.wav")
-        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True,
-                stream_callback=callback)
+# count = 0
+# toggle_sound = 1
+# while stream.is_active():
+#     count +=1
+#     print(count)
+#     time.sleep(0.18)
+#     # stream.stop_stream()
+#     # stream.close()
+#     # wf.close()
+#     if toggle_sound == 1:
+#         toggle_sound = 2
+#         stream.stop_stream()
+#         stream.close()
+#         wf.close()
+#     else:
+#         toggle_sound = 1
+#     wf = wave.open(f"violence_sound_{toggle_sound}.wav")
+#     stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+#             channels=wf.getnchannels(),
+#             rate=wf.getframerate(),
+#             output=True,
+#             stream_callback=callback)
     
 
 # stop stream
